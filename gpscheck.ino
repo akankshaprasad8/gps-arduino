824df4937eb281e5857df4d00721c72bd71c804f
akankshaprasad8 <52112581+akankshaprasad8@users.noreply.github.com>
2020-06-12 19:22:53 +0530

    Add files via upload

---
+++ gpscheck.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+// The serial connection to the GPS module
+SoftwareSerial ss(4, 3);
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(9600);
+}
+
+void loop(){
+  while (ss.available() > 0){
+    // get the byte data from the GPS
+    byte gpsData = ss.read();
+    Serial.print(gpsData);
+  }
+   byte gpsData = ss.read();
+       Serial.println(gpsData);
+}
